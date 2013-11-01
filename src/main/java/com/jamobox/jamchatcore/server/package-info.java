6ce93fb4d5a40ea0f760aaf5b7fc2ef5551f30af
JamoBox <petewicken@gmail.com>
2013-11-01 14:19:07 +0000

    Updated JavaDocs

---
+++ src/main/java/com/jamobox/jamchatcore/server/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Contains all classes that are directly related to the server connection.
+ * The Server class within this package must always be extended by any instance of
+ * a server that is implemented in a client. The package also contains classes of
+ * reading from and writing to the instance of the Server class.
+ *
+ * @see main.java.com.jamobox.jamchatcore.server.Server
+ */
+package main.java.com.jamobox.jamchatcore.server;
